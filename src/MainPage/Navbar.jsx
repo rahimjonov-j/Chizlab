07a885c0c95123bce99eaf37af1c54e64ea1fc7a
Javohir <javohirrahimjonov546@gmail.com>
2026-03-02 13:42:13 +0300

    Login icon change

--- src/MainPage/Navbar.jsx
+++ src/MainPage/Navbar.jsx
@@ -108,8 +108,8 @@ export default function Navbar() {
               <Tooltip>
                 <TooltipTrigger asChild>
                   <Link to="/login">
-                    <button className="px-3 py-1.5 cursor-pointer bg-blue-500 text-white font-semibold rounded-md">
-                      Tizimga kirish
+                    <button className="px-3 py-1.5 flex items-center gap-2 cursor-pointer bg-blue-500 text-white font-semibold rounded-md">
+                    <span><svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M4.5 1C4.22386 1 4 1.22386 4 1.5C4 1.77614 4.22386 2 4.5 2H12V13H4.5C4.22386 13 4 13.2239 4 13.5C4 13.7761 4.22386 14 4.5 14H12C12.5523 14 13 13.5523 13 13V2C13 1.44772 12.5523 1 12 1H4.5ZM6.60355 4.89645C6.40829 4.70118 6.09171 4.70118 5.89645 4.89645C5.70118 5.09171 5.70118 5.40829 5.89645 5.60355L7.29289 7H0.5C0.223858 7 0 7.22386 0 7.5C0 7.77614 0.223858 8 0.5 8H7.29289L5.89645 9.39645C5.70118 9.59171 5.70118 9.90829 5.89645 10.1036C6.09171 10.2988 6.40829 10.2988 6.60355 10.1036L8.85355 7.85355C9.04882 7.65829 9.04882 7.34171 8.85355 7.14645L6.60355 4.89645Z" fill="currentColor" fill-rule="evenodd" clip-rule="evenodd"></path></svg></span>  Tizimga kirish
                     </button>
                   </Link>
                 </TooltipTrigger>
@@ -120,8 +120,14 @@ export default function Navbar() {
             )}
             {/* user Profile  */}
             {isAuth && (
+
               <DropdownMenu>
-                <DropdownMenuTrigger asChild>
+                  <Tooltip>
+                <TooltipTrigger asChild>
+                       <DropdownMenuTrigger asChild>
+                  
+                    
+
                   <button className=" border-gray-800 rounded-full border-1 p-0.5 shadow-md dark:border-white">
                     <Avatar className="cursor-pointer bg-black text-white dark:bg-white dark:text-black">
                       <AvatarFallback>
@@ -130,6 +136,12 @@ export default function Navbar() {
                     </Avatar>
                   </button>
                 </DropdownMenuTrigger>
+                </TooltipTrigger>
+                <TooltipContent side="bottom">
+                  <p>Foydalanuvchi profiliga kirish uchun bosing!</p>
+                </TooltipContent>
+              </Tooltip>
+        
 
                 <DropdownMenuContent
                   align="end"
@@ -140,7 +152,7 @@ export default function Navbar() {
                   </DropdownMenuItem>
 
                   <DropdownMenuItem onClick={handleLogout}>
-                    <div className="flex items-center gap-2">
+                    <div className="flex items-center gap-2 ">
                       <span>
                         <svg className="text-red-700"
                           width="15"
